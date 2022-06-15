543f3bbbf30d023e0677d627af2bb29cf6f7b4e1
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2022-06-16 00:24:53 +0600

    first commit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
+
+function Homepage() {
+  return (
+    <div>
+      <DefaultLayout>
+        <h1>Hi</h1>
+      </DefaultLayout>
+    </div>
+  );
+}
+
+export default Homepage;
